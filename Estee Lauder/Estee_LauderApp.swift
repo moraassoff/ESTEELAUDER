0138480b587c4fe9d5bd57880d13395dfff401d8
moraassoff <moraassoff.scholar@gmail.com>
2023-07-26 23:04:44 -0600

    Initial Commit

---
+++ Estee Lauder/Estee_LauderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estee_LauderApp.swift
+//  Estee Lauder
+//
+//  Created by Sofia Mora Gómez on 26/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Estee_LauderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
